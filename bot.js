7c9138399b1712f58883a2c82473636484050ce1
生方 健成 <obukata@mj-inc.jp>
2017-06-28 13:47:01 +0900

    サンプル応答

--- bot.js
+++ bot.js
@@ -18,10 +18,30 @@ controller.spawn({
 });
 
 // say hi
-controller.hears('やあ',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
-    bot.reply(message,'なんだよ');
+controller.hears('チンプキン',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'何か呼んだ？:chipmunk:');
 });
 
 controller.hears('疲れた',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
-    bot.reply(message,'このひまわりの種でも食べる？');
+    bot.reply(message,'ひまわりの種でも食べる？');
+});
+
+controller.hears('お腹すいた',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'食べる？これ。');
+});
+
+controller.hears('柏',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'田舎の事？:thinking_face:');
+});
+
+controller.hears('頑張って',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'頑張るよー！:smiley:');
+});
+
+controller.hears('車運転',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'気をつけてね:slightly_smiling_face:');
+});
+
+controller.hears('到着',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+    bot.reply(message,'いえーい');
 });
