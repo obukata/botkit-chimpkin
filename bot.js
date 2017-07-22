c507815a2f8d707c279b96d365d5e5e84a8f8b92
obukata <squib02@gmail.com>
2017-07-22 20:49:57 +0900

    実装途中で挫折だよ！chimpkinクイズ

--- bot.js
+++ bot.js
@@ -129,12 +129,72 @@ controller.hears('パルミジャーノ',['direct_message','direct_mention','men
 	bot.reply(message,'*レッジャーノ!!*:spaghetti:');
 });
 
+
 //=========================================================
 // chimpkin 今何年？
 //=========================================================
 controller.hears('今(.*)何年',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
 	bot.reply(message,'今は' + chimpkinDate_Y + '年で\n平成' + (chimpkinDate_Y - 1988) + '年だよー。');
 });
+
+
+//=========================================================
+// chimpkin クイズ
+//=========================================================
+// controller.hears('クイズ',['direct_message','direct_mention','mention'],function(bot,message) {
+// 	bot.startConversation(message, function (err, convo) {
+// 		http.get("http://api.quizken.jp/api/captcha/format/json", (response) => {
+// 			let body = '';
+// 			response.setEncoding('utf8').on('data', (chunk) => {  body += chunk;  });
+// 			response.on('end', () => {
+// 				let current = JSON.parse(body);
+// 				var chimpkinQuiz = current['correctAnswerIndex'];
+// 				var chimpkinQuizAnsers = current['answers'][chimpkinQuiz];
+// 				var chimpkinAnswers =  0  + ' : ' + current['answers'][0] + '\n'; 
+// 				for(var i = 1; i < current['answers'].length; i++) {
+// 					chimpkinAnswers = chimpkinAnswers + i  + ' : ' + current['answers'][i] + '\n'; 
+// 				}
+// 				let text =
+// 				'問題！\n' + current['question'] + '\n' +
+// 				'```' +
+// 				chimpkinAnswers +
+// 				'```';
+// 				// convo.ask() で質問をします。
+// 				convo.ask(text, [{
+// 					pattern: ['(.*)'],
+// 					callback: function (response, convo) {
+// 					var chimpkinChoice = message.match[1];
+// 						if(chimpkinChoice == chimpkinQuiz) {
+// 							convo.say(message, '正解ー！:o:');
+// 						}else {
+// 							convo.say(message, '不正解ー！:x:\n正解は' + chimpkinQuizAnsers + 'でしたー！');
+// 						}
+// 					}
+// 					// convo.next(); // 会話を次に進めます。この場合、最初の質問にも戻ります。
+// 				}, {
+// 					default: true,
+// 					callback: function (response, convo) {
+// 						// ▼ どのパターンにもマッチしない時の処理 ▼
+// 						convo.say('不正解ー！:x:\n正解は' + chimpkinQuizAnsers + 'でしたー！');
+// 						convo.next(); // 会話を次に進めます。この場合、最初の質問にも戻ります。
+// 					}
+// 				}]);
+// 			});
+// 		// controller.hears(['(.*)'],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+// 		// 		var chimpkinChoice = message.match[1];
+// 		// 		console.log(chimpkinChoice);
+// 		// 		console.log(chimpkinQuiz);
+// 		// 		if(chimpkinChoice == chimpkinQuiz) {
+// 		// 			bot.reply(message, '正解ー！:o:');
+// 		// 		}else {
+// 		// 			bot.reply(message, '不正解ー！:x:\n正解は' + chimpkinQuizAnsers + 'でしたー！');
+// 		// 		}
+// 		// 	});
+// 		});
+// 	});
+// });
+
+
 
 //=========================================================
 // chimpkin おみくじ
