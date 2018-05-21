bd74a6cdab6d7e6c82d5ab1ed6520163fab1508a
生方 健成 <obukata@mj-inc.jp>
2018-05-21 12:36:11 +0900

    最小セットでテスト。

--- module/talk.js
+++ module/talk.js
@@ -17,6 +17,34 @@ module.exports = controller => {
 // 	}
 // })
 
+const getRandom = function(array) {
+	if(!array) {
+		return
+	} else {
+		return array[Math.floor(Math.random() * array.length)]
+	}
+}
+
+	let maccoto = {
+		id: 'U5MPH15RU',
+		name: 'ご主人',
+		birthday: '19880828',
+		like: {
+			ramen: ['河童ラーメン', '天下一品', '中華そば 葛', 'ふく流ラパス 分家 ワダチ', '鶏Soba 座銀']
+		}
+	}
+
+	let kichi = {
+		id: 'U5M2E7GTW',
+		name: ['みゆきち', 'あきお', 'きっち', 'きち子', 'きちみゆ', 'こーきちろう', 'きち', 'きっちゃん'],
+		birthday: '19801226',
+		like: {
+			sanuki: {
+				dinner: ['日高屋', '木屋', '四季亭', '四季蕎麦', 'カリカ', '紅光', 'Grano', 'サガル', 'しちりん']
+			}
+		}
+	}
+
 
 	controller.hears('チンプキン',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
 		bot.reply(message, getRandom([
