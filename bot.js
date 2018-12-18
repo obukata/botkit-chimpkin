59a994070d52d5cbb26798a77d1ee250540cbd5f
obukata <obukata@mj-inc.jp>
2018-12-18 12:50:12 +0900

    interactive_message_callback、古いbotkitだと正しく動いてないらしいけど…？

--- bot.js
+++ bot.js
@@ -29,12 +29,21 @@ async = require('async')
 if (!process.env.token) {
 	console.log('Error: Specify token in environment')
 	process.exit(1)
+}else if(!process.env.port) {
+	console.log('Error: Specify port in environment')
+	process.exit(1)
 }
 
 controller = Botkit.slackbot({
 	debug: false,
 	json_file_store: 'storage_bot_db'
-})
+}).configureSlackApp(
+	{
+		clientId: process.env.clientId,
+		clientSecret: process.env.clientSecret,
+		scopes: ['bot'],
+	}
+)
 
 controller.spawn({
 	token: process.env.token
@@ -44,6 +53,16 @@ controller.spawn({
 	}
 })
 
+controller.setupWebserver(process.env.port,function(err,webserver) {
+	controller.createWebhookEndpoints(controller.webserver);
+	controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+		if (err) {
+			res.status(500).send('ERROR: ' + err);
+		} else {
+			res.send('Success!');
+		}
+	})
+})
 
 const libraryPath = Path.resolve(__dirname, 'library')
 Fs.readdir(libraryPath, (err, list) => {
@@ -67,32 +86,51 @@ Fs.readdir(modulePath, (err, list) => {
 
 controller.hears('button', ['direct_message'],function(bot,message) {
 	var reply = {
-		"text": "ボタンのテストです。",
-		"attachments": [{
-			"text": "どれか押してください。",
-			"fallback": "失敗しました。",
-			"callback_id": "test_button",
-			"color": "#808080",
-			"actions": [
-				{
-					"type": "button",
-					"name": "test_button1",
-					"text": "テストボタン1"
-				},
-				{
-					"type": "button",
-					"name": "test_button2",
-					"text": "テストボタン2"
-				}
-			]
-		}]
-	};
+		"text": "Would you like to play a game?",
+		"attachments": [
+			{
+				"text": "Choose a game to play",
+				"fallback": "You are unable to choose a game",
+				"callback_id": "wopr_game",
+				"color": "#3AA3E3",
+				"attachment_type": "default",
+				"actions": [
+					{
+						"name": "game",
+						"text": "Chess",
+						"type": "button",
+						"value": "chess"
+					},
+					{
+						"name": "game",
+						"text": "Falken's Maze",
+						"type": "button",
+						"value": "maze"
+					},
+					{
+						"name": "game",
+						"text": "Thermonuclear War",
+						"style": "danger",
+						"type": "button",
+						"value": "war",
+						"confirm": {
+							"title": "Are you sure?",
+							"text": "Wouldn't you prefer a good game of chess?",
+							"ok_text": "Yes",
+							"dismiss_text": "No"
+						}
+					}
+				]
+			}
+		]
+	}
 	bot.reply(message, reply);
 })
 controller.hears('ローカル？', ['direct_message','direct_mention','mention','ambient'],function(bot,message) {
 	bot.reply(message, 'ローカライゼーション！')
 })
 controller.on('interactive_message_callback', function(bot, message) {
+	console.log('test')
 	bot.reply(message, 'ボタン押した！')
 	var users_answer = message.actions[0].name;
 	if (message.callback_id == "test_button") {
