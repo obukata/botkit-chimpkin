f2a7c348f1e758f968185dcfb1f20b6e5b963202
obukata <obukata@mj-inc.jp>
2018-12-26 11:51:44 +0900

    ファイル間またぐ時はこう？

--- module/interactive_message_callback.js
+++ module/interactive_message_callback.js
@@ -6,12 +6,19 @@
 'use strict'
 module.exports = controller => {
 
+	require('./orientalZodiac.js')(controller)
+
 	controller.on('interactive_message_callback', function(bot, message) {
 		if (message.callback_id == 'info') {
 			data.bot = bot
 			data.message = message
 			orientalZodiacNow(data)
+			buttonSend(data)
 		}
 	})
+
+	function buttonSend(data) {
+		data.bot.reply(data.message, '押したよ！')
+	}
 
 }
