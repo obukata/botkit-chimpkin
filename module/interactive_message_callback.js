bb66d3391a2848b6111f754a757ce32c2c69aaef
obukata <obukata@mj-inc.jp>
2018-12-26 17:40:08 +0900

    こういう事？

--- module/interactive_message_callback.js
+++ module/interactive_message_callback.js
@@ -47,7 +47,7 @@ module.exports = controller => {
 					}]
 				})
 			}else if(message.actions[0].name == 'horoscopes_list') {
-				const auguryNum = message.action[0].options.value
+				const auguryNum = message.actions[0].options.value
 				horoscopes(data, auguryNum)
 			}
 		}
