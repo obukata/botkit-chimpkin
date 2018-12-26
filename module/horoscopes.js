9f20f4c1a1203186d6553474c674592a3b14e347
obukata <obukata@mj-inc.jp>
2018-12-26 18:45:10 +0900

    こういう事…じゃないよね？

--- module/horoscopes.js
+++ module/horoscopes.js
@@ -62,11 +62,16 @@ module.exports = controller => {
 				response.on('end', () => {
 					let current = JSON.parse(body)
 					let text =
-					current['horoscope'][auguryNowDate][auguryNum]['sign'] + 'の今日の運勢\n' +
-					current['horoscope'][auguryNowDate][auguryNum]['content'] + '\n' +
-					'> :moneybag:金運　：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['money']] + '\n' +
-					'> :briefcase:仕事運：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['job']] + '\n' +
-					'> :heart:恋愛運：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['love']]
+					':crown:' + current.horoscope.auguryNowDate.auguryNum.rank + '位：' + current.horoscope.auguryNowDate.auguryNum.sign. + 'の今日の運勢\n' +
+					current.horoscope.auguryNowDate.auguryNum.content. + '\n' +
+					'> :moneybag:金運　：' + asterismLuck[current.horoscope.auguryNowDate.auguryNum.money] + '\n' +
+					'> :briefcase:仕事運：' + asterismLuck[current.horoscope.auguryNowDate.auguryNum.job] + '\n' +
+					'> :heart:恋愛運：' + asterismLuck[current.horoscope.auguryNowDate.auguryNum.love]
+					// ':crown:' + current['horoscope'][auguryNowDate][auguryNum]['rank'] + '位：' + current['horoscope'][auguryNowDate][auguryNum]['sign'] + 'の今日の運勢\n' +
+					// current['horoscope'][auguryNowDate][auguryNum]['content'] + '\n' +
+					// '> :moneybag:金運　：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['money']] + '\n' +
+					// '> :briefcase:仕事運：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['job']] + '\n' +
+					// '> :heart:恋愛運：' + asterismLuck[current['horoscope'][auguryNowDate][auguryNum]['love']]
 					horoscopesView(data, text)
 				})
 			})
