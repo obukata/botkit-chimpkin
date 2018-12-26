02d391387d04efcbbe820c8de122909e427408f1
obukata <obukata@mj-inc.jp>
2018-12-26 17:42:00 +0900

    送られてくるのも配列0？

--- module/interactive_message_callback.js
+++ module/interactive_message_callback.js
@@ -47,7 +47,7 @@ module.exports = controller => {
 					}]
 				})
 			}else if(message.actions[0].name == 'horoscopes_list') {
-				const auguryNum = message.actions[0].options.value
+				const auguryNum = message.actions[0].options[0].value
 				horoscopes(data, auguryNum)
 			}
 		}
