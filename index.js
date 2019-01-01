04af0cb0a79393b3dbb1dac7ec36082facf4df37
生方健成 <squib02@gmail.com>
2019-01-01 15:43:55 +0900

    一旦止めてみる。

--- index.js
+++ index.js
@@ -77,41 +77,38 @@ controller.on('create_bot',function(bot,config) {
 				if (err) {
 					console.log(err)
 				} else {
-					convo.say('I am a bot that has just joined your team')
-					convo.say('You must now /invite me to a channel so that I can be of use!')
+					convo.say('やあ')
 				}
 			})
 		})
 	}
 })
 
-// Handle events related to the websocket connection to Slack
-controller.on('rtm_open',function(bot) {
-	console.log('** The RTM api just connected!')
-})
-controller.on('rtm_close',function(bot) {
-	console.log('** The RTM api just closed')
-	// you may want to attempt to re-open
-})
+// // Handle events related to the websocket connection to Slack
+// controller.on('rtm_open',function(bot) {
+// 	console.log('** The RTM api just connected!')
+// })
+// controller.on('rtm_close',function(bot) {
+// 	console.log('** The RTM api just closed')
+// 	// you may want to attempt to re-open
+// })
 
-const libraryPath = Path.resolve(__dirname, 'library')
-Fs.readdir(libraryPath, (err, list) => {
-	for (const file of list) {
-		if(file.match('.js')) {
-			const pluginPath = Path.resolve(libraryPath, file)
-			require(pluginPath)(controller)
-		}
-	}
-})
+// const libraryPath = Path.resolve(__dirname, 'library')
+// Fs.readdir(libraryPath, (err, list) => {
+// 	for (const file of list) {
+// 		if(file.match('.js')) {
+// 			const pluginPath = Path.resolve(libraryPath, file)
+// 			require(pluginPath)(controller)
+// 		}
+// 	}
+// })
 
-const modulePath = Path.resolve(__dirname, 'module')
-Fs.readdir(modulePath, (err, list) => {
-	for (const file of list) {
-		if(file.match('.js')) {
-			const pluginPath = Path.resolve(modulePath, file)
-			require(pluginPath)(controller)
-		}
-	}
-})
-
-
+// const modulePath = Path.resolve(__dirname, 'module')
+// Fs.readdir(modulePath, (err, list) => {
+// 	for (const file of list) {
+// 		if(file.match('.js')) {
+// 			const pluginPath = Path.resolve(modulePath, file)
+// 			require(pluginPath)(controller)
+// 		}
+// 	}
+// })
